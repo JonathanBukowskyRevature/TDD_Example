64bd006f2788752b1d7abda864455461bdc215ec
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-10-18 00:00:56 -0400

    Add initial project

---
+++ TDD_Example/ExampleImplementation/Fibonacci/Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleImplementation.Fibonacci
+{
+  public abstract class Fibonacci
+  {
+    /// <summary>
+    /// Get the nth fibonacci number (starting from 0)
+    /// </summary>
+    /// <param name="n"></param>
+    public abstract long GetFibonacci(int n);
+  }
+
+}
---
+++ TDD_Example/ExampleImplementation/Shapes/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleImplementation.Shapes
+{
+  public abstract class Shape
+  {
+    protected Shape() { }
+    public abstract double GetArea();
+  }
+
+  public abstract class ShapeFactory
+  {
+    public abstract Shape Factory();
+  }
+
+}
