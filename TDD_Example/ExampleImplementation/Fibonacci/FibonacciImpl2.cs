8d2748d68bb1589cd6007b50b58a0e3876bd6986
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-10-18 01:36:33 -0400

    Added more implementations

---
+++ TDD_Example/ExampleImplementation/Fibonacci/FibonacciImpl2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleImplementation.Fibonacci
+{
+  public class FibonacciImpl2 : Fibonacci
+  {
+    public override long GetFibonacci(int n)
+    {
+      if (n == 0)
+      {
+        return 0;
+      } else if (n == 1)
+      {
+        return 1;
+      }
+      return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+    }
+  }
+}
---
+++ TDD_Example/ExampleImplementation/Fibonacci/FibonacciImpl3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleImplementation.Fibonacci
+{
+  public class FibonacciImpl3 : Fibonacci
+  {
+
+    private List<long> _memo = new() { 0, 1 };
+
+    public override long GetFibonacci(int n)
+    {
+      while (n > _memo.Count())
+      {
+        _memo.Add(_memo[_memo.Count - 1] +_memo[_memo.Count - 2]);
+      }
+      return _memo[n];
+    }
+  }
+}
