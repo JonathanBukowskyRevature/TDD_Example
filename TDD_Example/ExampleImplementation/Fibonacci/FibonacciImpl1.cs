773f220ca7edffef7cb28af0fcad5d14c68a7fc8
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-10-18 01:03:47 -0400

    Add empty implementation class for fibonacci

---
+++ TDD_Example/ExampleImplementation/Fibonacci/FibonacciImpl1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleImplementation.Fibonacci
+{
+  public class FibonacciImpl1 : Fibonacci
+  {
+    public override long GetFibonacci(int n)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
